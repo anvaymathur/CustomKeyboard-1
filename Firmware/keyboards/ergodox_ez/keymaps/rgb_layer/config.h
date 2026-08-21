77060dbf8f8e65790700ec853d107aeaf7794cc3
Anvay Mathur <168945509+anvaymathur@users.noreply.github.com>
2026-08-20 21:05:56 -0400

    Add QMK fork submodule for anvay100keyboard firmware
    This is where the actual code is written. Go to Keyboards -> anvay100keyboard for my firmware code. - 4 hours

---
+++ Firmware/keyboards/ergodox_ez/keymaps/rgb_layer/config.h
@@ -0,0 +1,18 @@
+#ifndef KEYMAP_CONFIG_H
+#define KEYMAP_CONFIG_H
+
+
+#define RGBLIGHT_SLEEP
+
+
+#undef PERMISSIVE_HOLD
+
+
+#define NKRO_DEFAULT_ON true
+
+#ifndef TAPPING_TOGGLE
+#define TAPPING_TOGGLE  1
+#endif
+
+#endif // !USERSPACE_CONFIG_H
+
